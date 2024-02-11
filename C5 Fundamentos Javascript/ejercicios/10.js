14129486600d6f1c06c1f3110a9df78fc3312d38
almen <aldo.mmendez@gmail.com>
2024-02-11 16:49:03 -0300

    Homework c05 hasta 09 resuelto

--- C5 Fundamentos Javascript/ejercicios/10.js
+++ C5 Fundamentos Javascript/ejercicios/10.js
@@ -3,6 +3,26 @@ function esFechaValida(fecha) {
   // Comprueba si este corresponde a una fecha válida.
   // Si es así, retorna true, sino retorna false.
   // Tu código:
+  if (typeof fecha === "string") return false;
+
+  //if (Date.Parse(fecha) === NaN) return false;
+  //var fechaObj = new Date(fecha);
+
+  var dia = fechaObj.getDate();
+  var mes = fechaObj.getMonth() + 1;
+  var año = fechaObj.getFullYear();
+  if (dia < 1 || dia > 31) return false;
+  if (mes < 1 || mes > 12) return false;
+  if (año <= 0) return false;
+
+  return true;
+
 }
+
+/* var fecha = "invalid"
+var fechaObj = new Date(fecha);
+
+console.log((fechaObj));
+console.log(typeof (fecha)); */
 
 module.exports = esFechaValida;
